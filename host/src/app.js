fde5810f2687a0a118278713cc40b265e0a697c3
AlMubaraq7 <momohmubaraq@gmail.com>
2024-08-18 08:18:28 +0100

    added build to server

--- host/src/app.js
+++ host/src/app.js
@@ -5,11 +5,19 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://denim-dreams.vercel.app",
+    origin: ["https://denim-dreams.vercel.app", "http://localhost:5173"],
   })
 );
 app.use(paymentRouter);
 app.get("/", (req, res) => {
   res.send("<h1>DenimDreams</h1>");
 });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "..", "build")));
+
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  });
+}
+
 module.exports = { app };
