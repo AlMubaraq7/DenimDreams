6099ecb5b79daf50dde9f03b3e8c36680e27040f
AlMubaraq7 <momohmubaraq@gmail.com>
2024-08-18 08:44:49 +0100

    hope

--- host/src/app.js
+++ host/src/app.js
@@ -12,12 +12,5 @@ app.use(paymentRouter);
 app.get("/", (req, res) => {
   res.send("<h1>DenimDreams</h1>");
 });
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "build")));
-
-  app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
-  });
-}
 
 module.exports = { app };
